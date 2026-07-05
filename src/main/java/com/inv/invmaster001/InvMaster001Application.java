7cec6bb70a2c13ee5a54f4e331e1f960b0718367
jayanthkrishnamurthy24 <jayanthkrishnamurthy24@gmail.com>
2026-07-05 21:03:07 +0530

    chore: add docker postgres + flyway migration setup for auto schema creation

---
+++ src/main/java/com/inv/invmaster001/InvMaster001Application.java
@@ -0,0 +1,11 @@
+package com.inv.invmaster001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvMaster001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(InvMaster001Application.class, args);
+    }
+}
